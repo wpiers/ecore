f0b07d86147c360592430f1feeb588931da86e07
William Piers <william.piers@obeo.fr>
2015-07-10 16:51:59 +0200

    fixed travis build

--- clouddesigner/org.occiware.clouddesigner.parent/buildAndDeploy.sh
+++ clouddesigner/org.occiware.clouddesigner.parent/buildAndDeploy.sh
@@ -1,8 +1,16 @@
-cd clouddesigner/org.occiware.clouddesigner.parent
+NIGHTLY_DIR=https://api.bintray.com/content/wpiers/generic/CloudDesigner/nightly
+
+# build clouddesigner
 mvn clean verify
 cd ../org.occiware.clouddesigner.product/target/products
-NIGHTLY_DIR=https://api.bintray.com/content/wpiers/generic/CloudDesigner/nightly
+
+# clear last nightly
+curl -v -uwpiers:$KEY $NIGHTLY_DIR -Q "DELE *"
+
+# copy new artifacts
 curl -T org.occiware.clouddesigner.product-linux.gtk.x86_64.zip -uwpiers:$KEY $NIGHTLY_DIR/CloudDesigner.linux.gtk.x86_64.zip
 curl -T org.occiware.clouddesigner.product-macosx.cocoa.x86_64.zip -uwpiers:$KEY $NIGHTLY_DIR/CloudDesigner.macosx.cocoa.x86_64.zip
 curl -T org.occiware.clouddesigner.product-win32.win32.x86.zip -uwpiers:$KEY $NIGHTLY_DIR/CloudDesigner.win32.x86.zip
+
+# publish nightly
 curl -XPOST -uwpiers:$KEY $NIGHTLY_DIR/publish
