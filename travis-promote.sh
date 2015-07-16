6648553d8e448d1737ef180ab230ba96928d465f
William Piers <william.piers@obeo.fr>
2015-07-16 18:31:18 +0200

    Update travis-promote.sh

--- travis-promote.sh
+++ travis-promote.sh
@@ -2,11 +2,21 @@ BINTRAY_USER=wpiers
 BINTRAY_R=https://dl.bintray.com/$BINTRAY_USER/generic
 BINTRAY_W=https://api.bintray.com/content/$BINTRAY_USER/generic
 
-# clear last nightly
+# clear last nightly products 
 NIGHTLY_PATTERN=CloudDesigner.$VERSION.N
-for file in $(curl --list-only --silent -u$BINTRAY_USER:$KEY $BINTRAY_R | grep 'href="#'$NIGHTLY_PATTERN | sed 's/.*href="#//' | sed 's/".*//');
+for file in $(curl --list-only --silent -u$BINTRAY_USER:$KEY $BINTRAY_R/ | grep 'href="#'$NIGHTLY_PATTERN | sed 's/.*href="#//' | sed 's/".*//');
 do curl -u$BINTRAY_USER:$KEY -X DELETE $BINTRAY_W/$file
-done
+don
+
+# clear last nightly update site
+curl -u$BINTRAY_USER:$KEY -X DELETE $BINTRAY_W/CloudDesigner/$VERSION/artifacts.jar
+curl -u$BINTRAY_USER:$KEY -X DELETE $BINTRAY_W/CloudDesigner/$VERSION/content.jar
+for file in $(curl --list-only --silent -u$BINTRAY_USER:$KEY $BINTRAY_R/CloudDesigner/$VERSION/features/ | grep 'href="#' | sed 's/.*href="#//' | sed 's/".*//');
+do curl -u$BINTRAY_USER:$KEY -X DELETE $BINTRAY_W/$file
+don
+for file in $(curl --list-only --silent -u$BINTRAY_USER:$KEY $BINTRAY_R/CloudDesigner/$VERSION/plugins/ | grep 'href="#' | sed 's/.*href="#//' | sed 's/".*//');
+do curl -u$BINTRAY_USER:$KEY -X DELETE $BINTRAY_W/$file
+don
 
 # copy new products
 VERSION_DIR=$BINTRAY_W/CloudDesigner/$VERSION
@@ -20,10 +30,10 @@ cd ../../../org.occiware.clouddesigner.repository/target/repository
 curl -T artifacts.jar -u$BINTRAY_USER:$KEY $VERSION_DIR/artifacts.jar
 curl -T content.jar -u$BINTRAY_USER:$KEY $VERSION_DIR/content.jar
 for file in $(features/*);
-curl -T $f -u$BINTRAY_USER:$KEY $VERSION_DIR/$f
+curl -T $file -u$BINTRAY_USER:$KEY $VERSION_DIR/$file
 done
 for file in $(plugins/*);
-curl -T $f -u$BINTRAY_USER:$KEY $VERSION_DIR/$f
+curl -T $file -u$BINTRAY_USER:$KEY $VERSION_DIR/$file
 done
 
 # publish nightly
