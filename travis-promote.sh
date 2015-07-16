f3b0b11f78f84f0606482bda842ddcd684590b46
William Piers <william.piers@obeo.fr>
2015-07-16 18:59:14 +0200

    Update travis-promote.sh

--- travis-promote.sh
+++ travis-promote.sh
@@ -6,17 +6,17 @@ BINTRAY_W=https://api.bintray.com/content/$BINTRAY_USER/generic
 NIGHTLY_PATTERN=CloudDesigner.$VERSION.N
 for file in $(curl --list-only --silent -u$BINTRAY_USER:$KEY $BINTRAY_R/ | grep 'href="#'$NIGHTLY_PATTERN | sed 's/.*href="#//' | sed 's/".*//');
 do curl -u$BINTRAY_USER:$KEY -X DELETE $BINTRAY_W/$file
-don
+done
 
 # clear last nightly update site
 curl -u$BINTRAY_USER:$KEY -X DELETE $BINTRAY_W/CloudDesigner/$VERSION/artifacts.jar
 curl -u$BINTRAY_USER:$KEY -X DELETE $BINTRAY_W/CloudDesigner/$VERSION/content.jar
 for file in $(curl --list-only --silent -u$BINTRAY_USER:$KEY $BINTRAY_R/CloudDesigner/$VERSION/features/ | grep 'href="#' | sed 's/.*href="#//' | sed 's/".*//');
 do curl -u$BINTRAY_USER:$KEY -X DELETE $BINTRAY_W/$file
-don
+done
 for file in $(curl --list-only --silent -u$BINTRAY_USER:$KEY $BINTRAY_R/CloudDesigner/$VERSION/plugins/ | grep 'href="#' | sed 's/.*href="#//' | sed 's/".*//');
 do curl -u$BINTRAY_USER:$KEY -X DELETE $BINTRAY_W/$file
-don
+done
 
 # copy new products
 VERSION_DIR=$BINTRAY_W/CloudDesigner/$VERSION
@@ -26,14 +26,16 @@ curl -T org.occiware.clouddesigner.product-macosx.cocoa.x86_64.zip -u$BINTRAY_US
 curl -T org.occiware.clouddesigner.product-win32.win32.x86.zip -u$BINTRAY_USER:$KEY $VERSION_DIR/CloudDesigner.$VERSION.N$TRAVIS_BUILD_NUMBER.win32.x86.zip
 
 # copy new update site
+FEATURES=features/*
+PLUTINS=plugins/*
 cd ../../../org.occiware.clouddesigner.repository/target/repository
 curl -T artifacts.jar -u$BINTRAY_USER:$KEY $VERSION_DIR/artifacts.jar
 curl -T content.jar -u$BINTRAY_USER:$KEY $VERSION_DIR/content.jar
-for file in $(features/*);
-curl -T $file -u$BINTRAY_USER:$KEY $VERSION_DIR/$file
+for file in $FEATURES; 
+do curl -T $file -u$BINTRAY_USER:$KEY $VERSION_DIR/$file
 done
-for file in $(plugins/*);
-curl -T $file -u$BINTRAY_USER:$KEY $VERSION_DIR/$file
+for file in $PLUGINS; 
+do curl -T $file -u$BINTRAY_USER:$KEY $VERSION_DIR/$file
 done
 
 # publish nightly
